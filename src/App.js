b4dbe008dc8bcd81d44081103c3fa5ec4bae7e49
StellaWanja <stylades@gmail.com>
2021-06-20 18:01:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+//import components
+import AppState from "./components/appstate"; //allow use of context
+import Navbar from "./components/navbar";
+import Notes from "./components/notes";
+import Form from "./components/form";
+//import pages
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+import Home from './pages/Home';
+import Note from "./pages/Note";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <AppState>
+
+        <Navbar /> 
+
+        <Switch>
+
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+
+          <Route exact path='/register'>
+            <Register />
+          </Route>
+
+          <Route exact path='/notes'>
+            <Notes />
+          </Route>
+
+          <Route exact path='/note-item'>
+            <Note />
+          </Route>
+
+          <Route exact path='/add-new-item'>
+            <Form />
+          </Route>
+
+          {/* move to 404 error page if issue arises */}
+          <Route>
+            <Error />
+          </Route>
+
+        </Switch>
+      </AppState>
+    </BrowserRouter>
+  )
 }
 
 export default App;
